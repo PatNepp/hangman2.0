2fc3df53596aaaed4a574d0dc72e45fd73106e74
Patrick Nepp <ptnepp@gmail.com>
2023-01-14 10:58:20 -0600

    passing props to word and wrong letters

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Figure from './components/Figure';
 import Header from './components/Header';
@@ -6,14 +7,23 @@ import Popup from './components/Popup';
 import Word from './components/Word';
 import WrongLetters from './components/WrongLetters';
 
+const words = ['application', 'programming', 'interface', 'conglomerate', 'guacamole', 'elephant']
+
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+
 function App() {
+  const [playable, setPlayable] = useState(true)
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
-        <WrongLetters />
-        <Word />
+        <WrongLetters wrongLetters={wrongLetters}/>
+        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
       </div>
       <Popup />
       <Notification />
--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -9,15 +9,15 @@ const Figure = () => {
         <line x1="60" y1="20" x2="60" y2="220" />
         <line x1="25" y1="220" x2="95" y2="220" />
         {/* <!-- Head --> */}
-        <circle cx="140" cy="70" r="20" class="figure-part"/>
+        <circle cx="140" cy="70" r="20" className="figure-part"/>
         {/* <!-- Body --> */}
-        <line x1="140" y1="90" x2="140" y2="160" class="figure-part"/>
+        <line x1="140" y1="90" x2="140" y2="160" className="figure-part"/>
         {/* <!-- Arms --> */}
-        <line x1="110" y1="130" x2="140" y2="110" class="figure-part"/>
-        <line x1="140" y1="110" x2="170" y2="130" class="figure-part"/>
+        <line x1="110" y1="130" x2="140" y2="110" className="figure-part"/>
+        <line x1="140" y1="110" x2="170" y2="130" className="figure-part"/>
         {/* <!-- Legs --> */}
-        <line x1="120" y1="205" x2="140" y2="160" class="figure-part"/>
-        <line x1="140" y1="160" x2="160" y2="205" class="figure-part"/>
+        <line x1="120" y1="205" x2="140" y2="160" className="figure-part"/>
+        <line x1="140" y1="160" x2="160" y2="205" className="figure-part"/>
     </svg>
     )
 }
